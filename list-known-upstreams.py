9af24a6d7c3227ba918bb982d8115b0145cfdc54
Simon Cozens <simon@simon-cozens.org>
2024-04-29 15:21:18 +0100

    Find plain source files we can build

--- list-known-upstreams.py
+++ list-known-upstreams.py
@@ -20,9 +20,7 @@ if os.path.exists("cache.json"):
 else:
     repos = {}
 
-for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
-    #if "noto" in directory:
-    #    continue
+for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR + "/ofl/*")))):
     try:
         gf = GoogleFont(directory, GFDIR)
     except Exception as e:
@@ -30,7 +28,7 @@ for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
         continue
     this_repo = {}
     base_directory = os.path.basename(directory)
-    if base_directory in repos:
+    if base_directory in repos and repos[base_directory].get("source_files"):
         continue
     repos[os.path.basename(directory)] = this_repo
     try:
@@ -39,13 +37,13 @@ for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
         print(f"Upstream {gf.github_owner_repo} not found")
         continue
     if not upstream:
-        print("No upstream for "+base_directory)
+        print("No upstream for " + base_directory)
         continue
     this_repo["has_upstream"] = True
     this_repo["last_updated"] = upstream.updated_at.isoformat()
     real_upstream = upstream.owner.login, upstream.name
     # progress.set_description(real_upstream[0]+"/"+real_upstream[1])
-    upstream = this_repo["real_upstream"] = real_upstream[0]+"/"+real_upstream[1]
+    upstream = this_repo["real_upstream"] = real_upstream[0] + "/" + real_upstream[1]
     if not gf.upstream.get("repository_url") and not gf.metadata.source.repository_url:
         print(f"{base_directory} should have upstream {upstream}")
     repo = GITHUB.get_repo(upstream)
@@ -54,17 +52,21 @@ for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
     except:
         continue
 
-    configs = [
-        p for p in sources
-        if p.path.startswith("sources/conf") and (p.path.endswith(".yaml") or p.path.endswith(".yml"))
-    ]
+    configs = []
+    for source in sources:
+        path = source.path
+        if not path.startswith("sources/"):
+            continue
+        if not (path.endswith(".yaml") or path.endswith(".yml")):
+            continue
+        if "sources" not in source.decoded_content.decode("utf-8"):
+            continue
+        configs.append(yaml.load(source.decoded_content, Loader=Loader))
+
     if configs:
         this_repo["is_gfr"] = True
-        this_repo["source_files"]= []
-        config = yaml.load(configs[0].decoded_content, Loader=Loader)
-        if "sources" not in config:
-            print(f"{base_directory} ({upstream}) has weird config file {configs[0]}")
-            continue
+        this_repo["source_files"] = []
+        config = configs[0]
         for source in config["sources"]:
             if source.endswith(".designspace"):
                 this_repo["source_files"].append("designspace")
@@ -72,7 +74,9 @@ for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
                 this_repo["source_files"].append("ufo")
             elif source.endswith(".glyphs"):
                 try:
-                    glyphs_file = requests.get(repo.get_contents("sources/"+source).download_url).text
+                    glyphs_file = requests.get(
+                        repo.get_contents("sources/" + source).download_url
+                    ).text
                     if ".formatVersion = 3" in glyphs_file:
                         this_repo["source_files"].append("Glyphs 3")
                     else:
@@ -80,11 +84,37 @@ for directory in tqdm.tqdm(list(sorted(glob.glob(GFDIR+"/ofl/*")))):
                 except:
                     this_repo["source_files"].append("Missing Glyphs file")
             elif source.endswith(".glyphspackage"):
-                glyphs_file = requests.get(repo.get_contents("sources/"+source+"/fontinfo.plist").download_url).text
+                glyphs_file = requests.get(
+                    repo.get_contents(
+                        "sources/" + source + "/fontinfo.plist"
+                    ).download_url
+                ).text
                 if ".formatVersion = 3" in glyphs_file:
                     this_repo["source_files"].append("Glyphs 3 package")
                 else:
                     this_repo["source_files"].append("Glyphs 2 package")
+    else:
+        this_repo["is_gfr"] = False
+        # Perhaps it just has a few sources we can build
+        source_files = []
+
+        def find_all_sources(r, srclist, extension):
+            found_sources = []
+            for src in srclist:
+                if src.path.endswith(extension) and src.path.startswith("sources/"):
+                    found_sources.append(src.path)
+            if found_sources:
+                r["sources"] = found_sources
+                return True
+            return False
+
+        # Find all glyphs files
+        if find_all_sources(this_repo, sources, ".glyphs"):
+            pass
+        elif find_all_sources(this_repo, sources, ".designspace"):
+            pass
+        elif find_all_sources(this_repo, sources, ".ufo"):
+            pass
 
     json.dump(repos, open("cache.json", "w"))
 
@@ -96,14 +126,15 @@ for r in repos.values():
     for s in sources:
         source_types[s] += 1
 
-print(f"""
+print(
+    f"""
       
 Out of {len(repos)} google font families:
     {upstream_repos} ({int(upstream_repos/len(repos)*100)}%) have known and accessible upstreams
     {gfr_repos} appear to be based on the GFR
 
 Out of {source_types.total()} source files:
-""")
+"""
+)
 for source_type, count in source_types.most_common():
     print(f"    {source_type}: {count} ({int(count/source_types.total()*100)}%)")
-
